a26b5a978b15486a16fd39546ce3c34ddfbc5de3
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2023-12-17 17:00:06 -1000

    added some backend code

---
+++ backendCS/Employee.cs
@@ -0,0 +1,15 @@
+public class Employee
+{
+    private int _id;
+    private string _firstName;
+    private string _lastName;
+    
+    public Employee(int id, string firstname, string lastname)
+    {
+        _id = id;
+        _firstName = firstname;
+        _lastName = lastname;
+    }
+
+    
+}
